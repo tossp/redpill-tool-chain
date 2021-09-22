51b56c059d82c58e10ff7e408d9080e95eadda91
砼砼 <code@tossp.com>
2021-09-23 03:02:25 +0800

    chore(all): update to 0.9.1

--- redpill_tool_chain.sh
+++ redpill_tool_chain.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -eu
 
+function checkPreconditon(){
+    missing_tools=""
+    for tool in jq docker realpath sha256sum; do
+        if [ ! $(which ${tool} ) ];then
+            missing_tools+=" ${tool}"
+        fi
+    done
+    if [ ${#missing_tools} -gt 0 ]; then
+        echo "required tool(s) missing:$missing_tools. Please install them and run the command again!"
+        exit 1
+    fi
+}
+checkPreconditon
+
 function readConfig() {
     cat global_config.json
 }
@@ -12,13 +26,17 @@ function getValueByJsonPath(){
 }
 
 function buildImage(){
+    local KERNEL_SRC_FILENAME=$( [ "${COMPILE_WITH}" == "kernel" ] && echo "${KERNEL_FILENAME}" || echo "${TOOLKIT_DEV_FILENAME}")
+    local KERNEL_SRC_FILENAME_SHA256=$( [ "${COMPILE_WITH}" == "kernel" ] && echo "${KERNEL_DOWNLOAD_SHA256}" || echo "${TOOLKIT_DEV_DOWNLOAD_SHA256}")
+    checkFileSHA256Checksum "${DOWNLOAD_FOLDER}/${KERNEL_SRC_FILENAME}" "${KERNEL_SRC_FILENAME_SHA256}"
+
     [ "${USE_BUILDKIT}" == "true" ] && export DOCKER_BUILDKIT=1
-    docker build --file docker/Dockerfile --force-rm  --pull \
+    docker build --file docker/Dockerfile --force-rm \
         $( [ "${USE_BUILD_CACHE}" == "false" ] && echo "--no-cache" ) \
         --build-arg DOCKER_BASE_IMAGE="${DOCKER_BASE_IMAGE}" \
         --build-arg COMPILE_WITH="${COMPILE_WITH}" \
         --build-arg EXTRACTED_KSRC="${EXTRACTED_KSRC}" \
-        --build-arg KERNEL_SRC_FILENAME="$( [ "${COMPILE_WITH}" == "kernel" ] && echo "${KERNEL_FILENAME}" || echo "${TOOLKIT_DEV_FILENAME}")" \
+        --build-arg KERNEL_SRC_FILENAME="${KERNEL_SRC_FILENAME}" \
         --build-arg REDPILL_LKM_REPO="${REDPILL_LKM_REPO}" \
         --build-arg REDPILL_LKM_BRANCH="${REDPILL_LKM_BRANCH}" \
         --build-arg REDPILL_LOAD_REPO="${REDPILL_LOAD_REPO}" \
@@ -61,10 +79,23 @@ function runContainer(){
         echo "Local redpill-load path does not exist: ${LOCAL_RP_LOAD_PATH}"
         exit 1
     fi
+    if [[ "${USE_CUSTOM_BIND_MOUNTS}" == "true" ]]; then
+        NUMBER_OF_MOUNTS=$(getValueByJsonPath ". | length" "${CUSTOM_BIND_MOUNTS}")
+        for (( i=0; i<${NUMBER_OF_MOUNTS}; i++ ));do
+            HOST_PATH=$(getValueByJsonPath ".[${i}].host_path" "${CUSTOM_BIND_MOUNTS}")
+            CONTAINER_PATH=$(getValueByJsonPath ".[${i}].container_path" "${CUSTOM_BIND_MOUNTS}")
+            if [ ! -e $(realpath "${HOST_PATH}") ]; then
+                echo "Host path does not exist: ${HOST_PATH}"
+                exit 1
+            fi
+            BINDS+="--volume $(realpath ${HOST_PATH}):${CONTAINER_PATH} "
+        done
+    fi
     docker run --privileged --rm  $( [ "${CMD}" == "run" ] && echo " --interactive") --tty \
         --name redpill-tool-chain \
         --hostname redpill-tool-chain \
         --volume /dev:/dev \
+        $( [ "${USE_CUSTOM_BIND_MOUNTS}" == "true" ] && echo "${BINDS}") \
         $( [ "${LOCAL_RP_LOAD_USE}" == "true" ] && echo "--volume $(realpath ${LOCAL_RP_LOAD_PATH}):/opt/redpill-load") \
         $( [ -e "${USER_CONFIG_JSON}" ] && echo "--volume $(realpath ${USER_CONFIG_JSON}):/opt/redpill-load/user_config.json") \
         --volume ${REDPILL_LOAD_CACHE}:/opt/redpill-load/cache \
@@ -84,6 +115,19 @@ function downloadFromUrlIfNotExists(){
     if [ ! -e ${OUT_FILE} ]; then
         echo "Downloading ${MSG}"
         curl --progress-bar --location ${DOWNLOAD_URL} --output ${OUT_FILE}
+    fi
+}
+
+function checkFileSHA256Checksum(){
+    local FILE="${1}"
+    local EXPECTED_SHA256="${2}"
+    local SHA256_RESULT=$(sha256sum ${FILE})
+    if [ "${SHA256_RESULT%% *}" != "${EXPECTED_SHA256}" ];then
+        echo "The ${FILE} is corrupted, expected sha256 checksum ${EXPECTED_SHA256}, got ${SHA256_RESULT%% *}"
+        #rm -f "${FILE}"
+        #echo "Deleted corrupted file ${FILE}. Please re-run your action!"
+        echo "Please delete the file ${FILE} manualy and re-run your command!"
+        exit 1
     fi
 }
 
@@ -112,6 +156,8 @@ Check global_settings.json for settings.
 EOF
 }
 
+
+
 # mount-bind host folder with absolute path into redpill-load cache folder
 # will not work with relativfe path! If single name is used, a docker volume will be created!
 REDPILL_LOAD_CACHE=${PWD}/cache
@@ -130,6 +176,8 @@ AVAILABLE_IDS=$(getValueByJsonPath ".build_configs[].id" "${CONFIG}")
 AUTO_CLEAN=$(getValueByJsonPath ".docker.auto_clean" "${CONFIG}")
 USE_BUILD_CACHE=$(getValueByJsonPath ".docker.use_build_cache" "${CONFIG}")
 CLEAN_IMAGES=$(getValueByJsonPath ".docker.clean_images" "${CONFIG}")
+USE_CUSTOM_BIND_MOUNTS=$(getValueByJsonPath ".docker.use_custom_bind_mounts" "${CONFIG}")
+CUSTOM_BIND_MOUNTS=$(getValueByJsonPath ".docker.custom_bind_mounts" "${CONFIG}")
 
 if [ $# -lt 2 ]; then
     showHelp
@@ -158,11 +206,13 @@ if [ "${ID}" != "all"  ]; then
     TARGET_REVISION=$(getValueByJsonPath ".platform_version | split(\"-\")[2]" "${BUILD_CONFIG}")
     USER_CONFIG_JSON=$(getValueByJsonPath ".user_config_json" "${BUILD_CONFIG}")
     DOCKER_BASE_IMAGE=$(getValueByJsonPath ".docker_base_image" "${BUILD_CONFIG}")
-    KERNEL_DOWNLOAD_URL=$(getValueByJsonPath ".download_urls.kernel" "${BUILD_CONFIG}")
     COMPILE_WITH=$(getValueByJsonPath ".compile_with" "${BUILD_CONFIG}")
-    KERNEL_FILENAME=$(getValueByJsonPath ".download_urls.kernel | split(\"/\")[] | select ( . | endswith(\".txz\"))" "${BUILD_CONFIG}")
-    TOOLKIT_DEV_DOWNLOAD_URL=$(getValueByJsonPath ".download_urls.toolkit_dev" "${BUILD_CONFIG}")
-    TOOLKIT_DEV_FILENAME=$(getValueByJsonPath ".download_urls.toolkit_dev | split(\"/\")[] | select ( . | endswith(\".txz\"))" "${BUILD_CONFIG}")
+    KERNEL_DOWNLOAD_URL=$(getValueByJsonPath ".downloads.kernel.url" "${BUILD_CONFIG}")
+    KERNEL_DOWNLOAD_SHA256=$(getValueByJsonPath ".downloads.kernel.sha256" "${BUILD_CONFIG}")
+    KERNEL_FILENAME=$(getValueByJsonPath ".downloads.kernel.url | split(\"/\")[] | select ( . | endswith(\".txz\"))" "${BUILD_CONFIG}")
+    TOOLKIT_DEV_DOWNLOAD_URL=$(getValueByJsonPath ".downloads.toolkit_dev.url" "${BUILD_CONFIG}")
+    TOOLKIT_DEV_DOWNLOAD_SHA256=$(getValueByJsonPath ".downloads.toolkit_dev.sha256" "${BUILD_CONFIG}")
+    TOOLKIT_DEV_FILENAME=$(getValueByJsonPath ".downloads.toolkit_dev.url | split(\"/\")[] | select ( . | endswith(\".txz\"))" "${BUILD_CONFIG}")
     REDPILL_LKM_REPO=$(getValueByJsonPath ".redpill_lkm.source_url" "${BUILD_CONFIG}")
     REDPILL_LKM_BRANCH=$(getValueByJsonPath ".redpill_lkm.branch" "${BUILD_CONFIG}")
     REDPILL_LOAD_REPO=$(getValueByJsonPath ".redpill_load.source_url" "${BUILD_CONFIG}")
@@ -180,8 +230,11 @@ else
 fi
 
 case "${ACTION}" in
-    build)  downloadFromUrlIfNotExists "${KERNEL_DOWNLOAD_URL}" "${DOWNLOAD_FOLDER}/${KERNEL_FILENAME}" "Kernel"
-            downloadFromUrlIfNotExists "${TOOLKIT_DEV_DOWNLOAD_URL}" "${DOWNLOAD_FOLDER}/${TOOLKIT_DEV_FILENAME}" "Toolkit Dev"
+    build)  if [ "${COMPILE_WITH}" == "kernel" ];then
+                downloadFromUrlIfNotExists "${KERNEL_DOWNLOAD_URL}" "${DOWNLOAD_FOLDER}/${KERNEL_FILENAME}" "Kernel"
+            else
+                downloadFromUrlIfNotExists "${TOOLKIT_DEV_DOWNLOAD_URL}" "${DOWNLOAD_FOLDER}/${TOOLKIT_DEV_FILENAME}" "Toolkit Dev"
+            fi
             buildImage
             if [ "${AUTO_CLEAN}" == "true" ]; then
                 clean
