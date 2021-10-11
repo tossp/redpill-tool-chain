33086d38a30b873310491b3bc3ad439f7e1a2be5
砼砼 <code@tossp.com>
2021-10-12 03:23:29 +0800

    chore(all): update to v0.11.4

--- synoboot.sh
+++ synoboot.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+T='/dev/synoboot'
+S='images/redpill-DS[39]*.img'
+
 cd $(dirname $(readlink -f "$0"))
 
-S='images/redpill-*'
-T='/dev/synoboot'
 IMG_FILE=`ls -lt ${S} 2>/dev/null | awk 'NR==1{print $9}'`
 
 if [ ! -b "${T}" ];then
@@ -11,12 +12,12 @@ if [ ! -b "${T}" ];then
     exit 1
 fi
 
-
-if [ -f "${IMG_FILE}" ];then
-    echo -e "Boot image:\t\t${PWD}/${IMG_FILE}"
-    echo -e "Target block device:\t${T}"
-    dd if="${IMG_FILE}" of="${T}" bs=4M conv=nocreat oflag=sync status=progress
-else
+if [ ! -f "${IMG_FILE}" ];then
     echo -e "Is not a valid file:\t${IMG_FILE}"
     ls -lt ${S}
+    exit 1
 fi
+
+echo -e "Boot image:\t\t${PWD}/${IMG_FILE}"
+echo -e "Target block device:\t${T}"
+dd if="${IMG_FILE}" of="${T}" bs=4M conv=nocreat oflag=sync status=progress
